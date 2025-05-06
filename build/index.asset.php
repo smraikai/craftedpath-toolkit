d61da5b20abfec4f9c419a087b23075679e44fda
smraikai <twyman214+github@gmail.com>
2025-05-06 08:24:17 -0400

    refactor: Remove AI Content Generator block files and associated assets to streamline the CraftedPath Toolkit, enhancing maintainability and reducing complexity

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'afa243da29803785ff0c');
